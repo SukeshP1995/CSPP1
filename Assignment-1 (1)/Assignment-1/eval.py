6c03f2df6aaff2ae082cb9915676a101efc87600
SukeshP1995 <sukeshpabolu@gmail.com>
2018-07-27 00:02:37 +0530

    CSPP1 Assignment-1 -> 1of1 passed. pylint: 10/10

--- Assignment-1 (1)/Assignment-1/eval.py
+++ Assignment-1 (1)/Assignment-1/eval.py
@@ -1,11 +1,52 @@
-import os.path,subprocess
+import os.path, subprocess
 from subprocess import STDOUT,PIPE
+import re
 import sys
 import platform
 import os.path
 import hashlib
 import base64
 # import crypt
+def get_platform():
+    platforms = {
+        'linux1' : 'Linux',
+        'linux2' : 'Linux',
+        'darwin' : 'OS X',
+        'win32' : 'Windows'
+    }
+    if sys.platform not in platforms:
+        return sys.platform
+    return platforms[sys.platform]
+
+def check_git():
+    try:
+        run_proc = subprocess.Popen('git', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except Exception as e:
+        return False
+
+def check_if_repo():
+    run_proc = subprocess.Popen('git status', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = run_proc.communicate()
+    if stderr: return False
+    return True
+
+def check_if_user():
+    run_proc = subprocess.Popen('git config user.name', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = run_proc.communicate()
+    if stdout: return True
+    return False
+
+def runProcess(command, expr=None):
+    run_proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    proc_out = run_proc.stdout.read().decode('utf-8')
+    print(proc_out)
+    if expr:
+        proc_out = re.findall(expr, proc_out)
+        if proc_out:
+            tests_passed = int(float(proc_out[0][0]))
+            tests_total = int(float(proc_out[0][1]))
+            return (tests_passed, tests_total)
 
 def which_python():
     if (sys.version_info > (3, 0)):
@@ -90,6 +131,7 @@ def run_tests(inputs,outputs,extension):
             print(result[2]+"\n")
         print("----------------------------------------")
     print("Result: "+str(passed)+"/"+str(len(inputs))+" testcases passed.")
+    return (passed, len(inputs))
 
 inputs = []
 outputs = []
@@ -103,6 +145,16 @@ for root,dirs,files in os.walk('.'):
             outputs.append(file)
     break
 
+# if get_platform() == 'Windows':
+if not check_git():
+    raise Exception('git not available')
+
+if not check_if_repo():
+    raise Exception('You are not in git repo')
+
+if not check_if_user():
+    raise Exception('user not logged in')
+    
 inputs = sorted(inputs)
 outputs = sorted(outputs)
 
@@ -110,18 +162,25 @@ if len(sys.argv)==2 and os.path.isfile(sys.argv[1]):
     if sys.argv[1].endswith(".java"):
         program_name = sys.argv[1]
         extension = ".java"
-        run_tests(inputs,outputs,extension)
+        result = run_tests(inputs,outputs,extension)
+
     elif sys.argv[1].endswith(".py"):
         program_name = sys.argv[1]
         extension = ".py"
-        run_tests(inputs,outputs,extension)
+        result = run_tests(inputs,outputs,extension)
     elif sys.argv[1].endswith(".c"):
         program_name = sys.argv[1]
         extension = ".c"
-        run_tests(inputs,outputs,extension)
+        result = run_tests(inputs,outputs,extension)
     elif sys.argv[1] == "eval.py":
         print("eval.py cannot be passed as argument")
     else:
         print("Invalid Extension.\nPass only .java or .py files")
 else:
     print("File not found.\nPass a valid filename with extension as argument.\npython eval.py <filename>")
+
+cases, totalcases = result
+score, totalscore = runProcess("pylint Solution.py","Your code has been rated at (.*)/(.*) \(.*\)")
+path = os.getcwd().split('\\')
+msg = path[-3] +' '+ path[-1]
+runProcess("git commit -am \""+ msg +" -> " + str(cases) + "of" + str(totalcases) + " passed." + " pylint: " + str(score) + "/" + str(totalscore) + " \"")
